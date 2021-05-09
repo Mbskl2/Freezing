75dee76782a4bdd2e8018e31a7bf14ef418f2a2e
Michał Banach <michal_banach@outlook.com>
2021-05-09 13:36:00 +0200

    Implement Wrapper instead of Layer.

--- freezing.py
+++ freezing.py
@@ -7,40 +7,47 @@ Original file is located at
     https://colab.research.google.com/drive/18Mj2XYJ6GiDPYbiZ1jGp1QWsfYb7YSE2
 """
 
-!pip install tensorflow-gpu
-
+# Commented out IPython magic to ensure Python compatibility.
+# %tensorflow_version 2.x
 import tensorflow as tf
-print(tf.__version__)
-print(tf.keras.__version__)
-tf.test.gpu_device_name() # is GPU working
+device_name = tf.test.gpu_device_name()
+if device_name != '/device:GPU:0':
+  raise SystemError('GPU device not found')
+print('Found GPU at: {}'.format(device_name))
+
+# Commented out IPython magic to ensure Python compatibility.
+# %load_ext tensorboard
+
+import datetime
+!rm -rf ./logs/ # Clear any logs from previous runs
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
 """TODO:
 
 
-1.   Dodać obsługę warstw konwolucyjnych.
-
+1. Dodać obsługę warstw konwolucyjnych. (/)
+2. Nowe zestawy danych: https://www.tensorflow.org/datasets/catalog/kmnist , eurosat i higgs
+3. Tesnsorboard
+4. Zestaw + boosting + wariancja skuteczności/błędów dla jednego zestawu bosstingowego
 
 """
 
 import numpy as np
+import tensorflow as tf
 
-class FreezingLayer(tf.keras.layers.Layer):
+class Freezing(tf.keras.layers.Wrapper):
   def __init__(self, frozen_percentage, layer):
-    super().__init__()
-    self.layer = layer
+    if not isinstance(layer, tf.keras.layers.Layer):
+      raise ValueError(
+          'Please initialize `Freezing` layer with a '
+          '`tf.keras.layers.Layer` instance. You passed: {input}'.format(
+              input=layer))
     self.frozen_percentage = frozen_percentage
-
-  def build(self, input_shape):
-    self.layer.build(input_shape)   
-
-  def compute_output_shape(self, input_shape):
-    return self.layer.compute_output_shape(input_shape)
+    super().__init__(layer) 
 
   def call(self, input):
     return self.layer.call(input)
-
-  def get_config(self):
-    return self.layer.get_config()
 
   def save_weights(self):
     self.__validate_layer(self.layer)
@@ -48,19 +55,20 @@ class FreezingLayer(tf.keras.layers.Layer):
     self.mask = self.__create_mask(layer_weights)
     self.old_weights = layer_weights[self.mask]
 
+  def __validate_layer(self, layer):
+    if len(layer.get_weights()) != 2: # weights and biases
+      raise ValueError("Cannot freeze layers that don't have weights.")
+
+  def __create_mask(self, layer_weights):
+    # TODO: lepiej layer_weights.shape[0] czy *layer_weights[-1] ?
+    frozen_neurons = np.random.rand(layer_weights.shape[0]) < self.frozen_percentage 
+    mask = np.full(layer_weights.shape, False)
+    mask[frozen_neurons, :] = True
+    return mask
+
   def reset_weights(self):
     self.__set_weights_on_layer(self.layer, self.old_weights, self.mask)
 
-  def __validate_layer(self, layer):
-    assert len(layer.get_weights()) > 0, "Cannot freeze layers that don't have weights."
-    assert len(layer.get_weights()[0].shape) == 2, 'FreezingLayer is applicable only for layers with matrices of weights with 2 dimensions e.g not Convolutional layers.'
-
-  def __create_mask(self, layer):
-    frozen_neurons = np.random.rand(layer.shape[0]) < self.frozen_percentage 
-    mask = np.full(layer.shape, False)
-    mask[frozen_neurons, :] = True
-    return mask
-      
   def __set_weights_on_layer(self, layer, old_weights, mask):
     weights = layer.get_weights()
     weights[0][mask] = old_weights
@@ -68,17 +76,20 @@ class FreezingLayer(tf.keras.layers.Layer):
 
 class EnableFreezing(tf.keras.callbacks.Callback):
   def __init__(self, N = 1):
-    assert N > 0, "The N parameter has to be a positive integer."
+    if N < 1:
+      raise ValueError(
+          'The N parameter has to be a positive integer.'
+          'You passed: {input}'.format(input=N))
     self.N = N
 
   def save_weights(self):
     for layer in self.model.layers:
-      if isinstance(layer, FreezingLayer):
+      if isinstance(layer, Freezing):
         layer.save_weights()
 
   def reset_weights(self):
     for layer in self.model.layers:
-      if isinstance(layer, FreezingLayer):
+      if isinstance(layer, Freezing):
         layer.reset_weights()
 
   def on_train_batch_end(self, batch, logs=None):
@@ -121,21 +132,48 @@ y_test = tf.keras.utils.to_categorical(y_test, num_classes)
 # y_test.shape  #(10000, 10)
 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3), kernel_initializer='he_uniform')) # With 'glorot' conv layers sometimes don't learn
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3), kernel_initializer='he_uniform')) # With kern_init='glorot' conv layers sometimes don't learn
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Freezing(0.5, Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform')))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
-model.add(FreezingLayer(1.0, Dense(150, activation='relu')))
-# model.add(Dense(150, activation='relu'))
-model.add(FreezingLayer(1.0, Dense(120, activation='relu')))
-# model.add(Dense(120, activation='relu'))
-model.add(FreezingLayer(1.0, Dense(num_classes, activation='softmax')))
+model.add(Freezing(0.5, Dense(150, activation='relu')))
+model.add(Freezing(0.5, Dense(120, activation='relu')))
+model.add(Freezing(0.5, Dense(num_classes, activation='softmax')))
 
 model.compile(loss=tf.keras.losses.categorical_crossentropy,
                     optimizer=tf.keras.optimizers.Adam(),
                     metrics=['accuracy'])
 
-model.fit(x_train, y_train, batch_size=32, epochs=5, callbacks=[EnableFreezingEveryNEpochs()]) 
+model.fit(
+    x_train,
+    y_train, 
+    batch_size=32,
+    epochs=5, 
+    callbacks=[EnableFreezingEveryNEpochs(), tensorboard_callback]) 
 train_score = model.evaluate(x_train, y_train, verbose=0)
 test_score = model.evaluate(x_test, y_test, verbose=0)
 
 print(train_score, test_score)
+
+for layer in model.layers: # - delete
+  w = layer.get_weights() # - delete
+  print(len(w)) # - delete
+  for x in w: # - delete
+    print(x.shape) # - delete
+  print('**********') # - delete
+
+layer_weights = np.random.rand(5, 2)
+layer_weights
+
+frozen_neurons = np.random.rand(layer_weights.shape[1]) < 0.5 
+frozen_neurons
+
+mask = np.full(layer_weights.shape, False)
+mask
+
+mask[:, frozen_neurons] = True
+mask
+
+# Commented out IPython magic to ensure Python compatibility.
+# %tensorboard --logdir logs/fit
